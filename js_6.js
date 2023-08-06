3210d5eba67351c795e4e010112f2e9e99163548
Margulis162 <markmargulis@proton.me>
2023-08-06 11:49:48 -0400

    display function

---
+++ js_6.js
@@ -0,0 +1,20 @@
+function displayMessage() {
+
+    const body = docyment.body;
+
+    const panel = document.createElement("div");
+    panel.setAttribute("class", "msgBox");
+    body.appendChild(panel);
+
+    const msg = document.createElement("p");
+    msg.textContent = "This is the message";
+    panel.appendChild(msg);
+
+    const closeBtn = document.createElement("button");
+    closeBtn.textContent ="x";
+    
+    panel.appendChild(closeBtn);
+    
+    closeBtn.addEventListener("click", () => panel.parentNode.removeChild(panel),);
+
+}
